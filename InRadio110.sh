af561aeb4b64d3ceaa84c5b0058c6adb120de8da
vincent <vincent@myos.fr>
2018-09-27 21:37:44 +0200

    New Radio add

--- InRadio110.sh
+++ InRadio110.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
-log=$HOME/logs/
+log=$HOME/logs/Radio110.log
+echo "start $(date)">$log
+source="http://myos.fr:8000/"
+
+gettrack=$(curl -s $source | sed 's/<[^>]*>//g' | awk -F 'playing:' {'print $2'} | sed '/^$/d')
+
+echo $gettrack>>$log
+# What Radio110 is playing ? 
+
 while true
 do
 
-track1=$(curl -s http://myos.fr:8000/ | sed 's/<[^>]*>//g' | awk -F 'playing:' {'print $2'} | sed '/^$/d')
+track1=$(echo $gettrack)
 
-echo $track1
+echo "$track1"
 
 sleep 1
 
-track2=$(curl -s http://myos.fr:8000/ | sed 's/<[^>]*>//g' | awk -F 'playing:' {'print $2'} | sed '/^$/d')
+track2=$(echo $gettrack)
+echo "$track2"
 
-echo $track2
 
 if [ "$track2" != "$track1" ]
 then 
-	echo $track2 >>$log
+	echo "$track2 $(date +"%m_%d_%Y")" >>$log
 fi
+
 done
 
