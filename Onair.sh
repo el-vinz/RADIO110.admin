81590c1fb740b8200be0519d70665beb5472382c
vincent <vincent@myos.fr>
2018-10-06 13:01:10 +0200

    functions track_radio add

--- Onair.sh
+++ Onair.sh
@@ -3,36 +3,24 @@
 [ -d $HOME/.logs ] || mkdir $HOME/.logs
 log_dir=$HOME/.logs/
 
-meuh_track () {
-    radio="MEUH"
-    
+meuh_track () {      
     
     source="http://www.radiomeuh.com/playlist/"
 
     track=$(curl -s $source | grep '</td></tr>' | sed 's/<[^>]*>//g' |  sed -n 3p | sed -r 's/^.{14}//')
 
-    track="$radio $track $(date +"%m_%d_%Y %H_%M ")"
     echo "$track"
 
 }
 
-110_track () {
- 
-    source="http://myos.fr:8000/"
-
-    track=$(curl -s $source | sed 's/<[^>]*>//g' | awk -F 'playing:' {'print $2'} | sed '/^$/d')
-    track="R110 $track "
-    echo "$track"
-}
-
-110_function () {
+meuh_function () {
+            radio="MEUH" 
             stamp=$(date +"%m_%d_%Y %H_%M ")
-            track1=$(110_track)
-            
-            radio=$(echo $track1 | cut -d " " -f1 )
-            
+            track1=$(meuh_track)
+            echo $track1
             sleep 1
-            track2=$(110_track)
+            track2=$(meuh_track)
+            echo $track2
 
             if [ "$track1" != "$track2" ]
                         then
@@ -41,14 +29,25 @@ meuh_track () {
 
 }
 
-meuh_function () {
+
+
+110_track () {
+    
+    source="http://myos.fr:8000/"
+
+    track=$(curl -s $source | sed 's/<[^>]*>//g' | awk -F 'playing:' {'print $2'} | sed '/^$/d')
+    
+    echo "$track"
+}
+
+110_function () {
+            radio="R110"
             stamp=$(date +"%m_%d_%Y %H_%M ")
-            track1=$(meuh_track)
-            
-            radio=$(echo $track1 | cut -d " " -f1 )
-            
+            track1=$(110_track)
+            echo $track1 
             sleep 1
-            track2=$(meuh_track)
+            track2=$(110_track)
+            echo $track2
 
             if [ "$track1" != "$track2" ]
                         then
@@ -61,8 +60,6 @@ while true
 do
 
     110_function
-
-    sleep 1
 
     meuh_function
 
